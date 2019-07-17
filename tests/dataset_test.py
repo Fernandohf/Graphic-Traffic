5c50d4c800354840177e75daed98d0cc76a5379f
Fernando Henrique <nandohfernandes@gmail.com>
2019-07-16 23:02:29 -0300

    bug: fix repeated function

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -40,13 +40,6 @@ class TestVocDataset():
         img, target = next(iter_ds)
         assert (target[..., 4].max() == 1)
 
-    def test_outputs_5(self,):
-        cls_test = ['bus', 'car']
-        ds = VOCDetectionCustom()
-        iter_ds = iter(ds)
-        img, target = next(iter_ds)
-        assert (target[..., 4].max() == 1)
-
     def test_errors_1(self,):
         cls_test = ['xxx', 'bus', 'car', 'motorbike']
         ds = VOCDetectionCustom(classes=cls_test)
