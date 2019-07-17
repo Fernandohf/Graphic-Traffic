fa6f1f568b514e29a396863d46156fbaf91e7508
Fernando Henrique <nandohfernandes@gmail.com>
2019-07-16 23:01:21 -0300

    chore: minor changes

---
+++ tests/dataset_test.py
@@ -0,0 +1,59 @@
+import pytest
+import torch
+
+from dataset import VOCDetectionCustom
+
+
+class TestVocDataset():
+    """
+    Tests for CustomVOCDataset Class
+    """
+
+    def test_outputs_1(self,):
+        ds = VOCDetectionCustom()
+        iter_ds = iter(ds)
+        img, target = next(iter_ds)
+
+        assert (img.shape == (3, 448, 448) and
+                target.shape == (14, 14, 2, 27))
+
+    def test_outputs_2(self,):
+        cls_test = ['bicycle', 'bus', 'car', 'motorbike']
+        ds = VOCDetectionCustom(classes=cls_test)
+        iter_ds = iter(ds)
+        img, target = next(iter_ds)
+
+        assert (img.shape == (3, 448, 448) and
+                target.shape == (14, 14, 2, 9))
+
+    def test_outputs_4(self,):
+        cls_test = ['bus', 'car']
+        ds = VOCDetectionCustom()
+        iter_ds = iter(ds)
+        img, target = next(iter_ds)
+        assert (target[..., 4].max() == 1)
+
+    def test_outputs_5(self,):
+        cls_test = ['bus', 'car']
+        ds = VOCDetectionCustom()
+        iter_ds = iter(ds)
+        img, target = next(iter_ds)
+        assert (target[..., 4].max() == 1)
+
+    def test_outputs_5(self,):
+        cls_test = ['bus', 'car']
+        ds = VOCDetectionCustom()
+        iter_ds = iter(ds)
+        img, target = next(iter_ds)
+        assert (target[..., 4].max() == 1)
+
+    def test_errors_1(self,):
+        cls_test = ['xxx', 'bus', 'car', 'motorbike']
+        ds = VOCDetectionCustom(classes=cls_test)
+        iter_ds = iter(ds)
+        with pytest.raises(IndexError):
+            img, target = next(iter_ds)
+
+    def test_errors_2(self,):
+        with pytest.raises(FileNotFoundError):
+            ds = VOCDetectionCustom('/ddata')
