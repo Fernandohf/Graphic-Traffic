6e06c10c212349a1b596383677663e500da1a060
Fernando Henrique <nandohfernandes@gmail.com>
2019-07-19 00:34:04 -0300

    bug: fixed class dependencies

--- train.py
+++ train.py
@@ -1,10 +1,15 @@
 """
 Trainer for the model.
 """
+import os
 
 import torch
 import torch.functional as F
 from torch import nn, optim
+from torch.lr_scheduler import ReduceLROnPlateau
+from tqdm import tqdm_notebook
+
+from model import TinyYOLO, YoloV3Loss
 
 
 class Trainer():
@@ -12,21 +17,29 @@ class Trainer():
     Trainer to train the model.
     """
 
-    def __init__(self, model, train_data, test_data,
-                 criterion, optimizer, scheduler):
+    def __init__(self, train_dl, test_dl, model=TinyYOLO(),
+                 criterion=YoloV3Loss(), lr=0.001, lr_factor=.25
+                 patience=3):
+        self.train_dl = train_dl
+        self.test_dl = test_dl
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
+        self.scheduler = ReduceLROnPlateau(optimizer,
+                                           'min',
+                                           factor=lr_factor,
+                                           patience=patience)
         # check if CUDA is available
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.lr = lr
 
-    def train(model, optimizer, criterion, train_dataloader,
-              test_dataloader, n_epochs=3, init_val_loss=np.Inf,
-              print_every=50, train_device='cuda', init_epoch=1):
+    def train(self, n_epochs=1, batch_size=32, init_val_loss=np.Inf,
+              print_every=50, init_epoch=1):
         """
         Function to train the model
 
         Args:
-            model: Model used.
-            optimizer: Optimizer used.
-            criterion: Criterion used
+            batch_size: Batch size used
             train_dataloader: Dataloader used
             test_dataloader: Dataloader used
             n_epochs: Max number of epochs.
@@ -40,52 +53,49 @@ class Trainer():
         # Progress bar
         pbar_epochs = tqdm_notebook(
             range(init_epoch, n_epochs + init_epoch),
-            total=n_epochs, ncols=800)
+            total=n_epochs + init_epoch, ncols=900)
         for epoch in pbar_epochs:
             # keep track of training and validation loss
             train_loss = 0.0
-
             ###################
             # train the model #
             ###################
-            model.train()
-            total_train = len(train_dataloader.dataset)
-            pbar_train = tqdm_notebook(enumerate(train_dataloader, 1),
-                                       total=total_train // BATCH_SIZE,
+            self.model.train().to(self.device)
+            total_train = len(self.train_dl.dataset)
+            pbar_train = tqdm_notebook(enumerate(self.train_dl, 1),
+                                       total=total_train // batch_size,
                                        ncols=750)
             for c, (img, target) in pbar_train:
                 # Move tensors to GPU, if CUDA is available
-                img, target = img.to(train_device), target.to(train_device)
+                img, target = img.to(self.device), target.to(self.device)
                 # Clear the gradients of all optimized variables
                 optimizer.zero_grad()
                 # Batch size
                 bs = img.size(0)
                 # forward pass
-                pred = model(img)
+                pred = self.model(img)
                 # calculate the batch loss
                 # import ipdb; ipdb.set_trace() # debugging starts here
                 # Fix dimension
-                pred = pred.permute(0, 2, 3, 1)
-                loss = criterion(pred.float(), target.float())
+                loss = self.criterion(pred, target)
                 # backward pass: compute gradient of the loss
                 loss.backward()
                 # perform a single optimization step (parameter update)
-                optimizer.step()
+                self.optimizer.step()
                 # update training loss
                 train_loss += loss.item() * bs
                 # show partial results
                 if c % print_every == 0:
                     # print training statistics
                     pbar_train.set_description(
-                        'Batch: {:5d}/{:5d} Training Loss: {:2.6f}'.format(
+                        'Batch: {:3d}/{:3d} Training Loss: {:2.6f}'.format(
                             c,
-                            len(train_dataloader),
+                            len(self.train_dl),
                             train_loss / (bs * c)
                         ))
 
             # Validate model
-            valid_loss = validate_model(
-                model, criterion, test_dataloader, train_device)
+            valid_loss = validate_model()
             losses["valid"].append(valid_loss)
             # calculate average losses
             train_loss = train_loss / total_train
@@ -111,20 +121,15 @@ class Trainer():
                 epoch, n_epochs, train_loss, valid_loss, save_model))
 
             # Scheduler step
-            scheduler.step(valid_loss)
+            self.scheduler.step(valid_loss)
 
         # Return losses
-        return losses
+        self.losses = losses
 
-    def validate(model, criterion, data_loader):
+    # TODO - correct documentation
+    def validate_model(self):
         """
         Validate the given model on test data.
-
-        Args:
-            model: Model used.
-            criterion: Criterion used
-            data_loader: Dataloader used
-            train_device: device used for validation.
 
         Return:
             loss: Validation loss
@@ -135,23 +140,20 @@ class Trainer():
         # Initializa valid loss
         valid_loss = 0.0
         # Move to device
-        model = model.to(train_device)
-        total_valid = len(data_loader.dataset)
+        self.model = self.model.to(self.device)
+        total_valid = len(self.test_dl.dataset)
         # Evalutions mode
-        model.eval()
+        self.model.eval()
         with torch.no_grad():
-            for img, target in data_loader:
+            for img, target in self.test_dl:
                 # move tensors to GPU if CUDA is available
-                img, target = img.to(train_device), target.to(train_device)
+                img, target = img.to(self.device), target.to(self.device)
                 bs = img.size(0)
-
                 # Check cache
-                pred = model(img)
-                pred = pred.permute(0, 2, 3, 1)
-                # calculate the batch loss
-                loss = criterion(pred.float(), target.float())
-
-                # update average validation loss
+                pred = self.model(img)
+                # Calculate the batch loss
+                loss = self.criterion(pred, target)
+                # Update average validation loss
                 valid_loss += loss.item() * bs
         # Valid loss
         valid_loss = valid_loss / total_valid
@@ -172,6 +174,7 @@ class Trainer():
             directory: directory to save models.
 
         """
+        directory_name = os.path.join(directory, file_name)
         # Saves the model
         checkpoint = {"model_state_dict": model.state_dict(),
                       "optim_state_dict": optimizer.state_dict(),
@@ -180,10 +183,10 @@ class Trainer():
                       "train_loss": losses["train"],
                       "valid_loss": losses["valid"]}
         # Created directory
-        torch.save(checkpoint, file_name)
+        torch.save(checkpoint, directory_name)
 
     def load_checkpoint(self, model, optimizer, scheduler,
-                        losses, file_name, location='cpu'):
+                        losses, file_path, location='cpu'):
         """
         Load all info from last model.
 
@@ -192,11 +195,11 @@ class Trainer():
             optimizer: Initialized Optimizer.
             Scheduler: Initialized Scheduler.
             losses: Initialized Dict with the losses.
-            file_name: name of the saved file.
+            file_path: Relatice/full path to file.
             location: Where to load the model.
         """
         # Loads the model
-        checkpoint = torch.load(file_name, map_location=location)
+        checkpoint = torch.load(file_path, map_location=location)
 
         # Load in given objects
         model.load_state_dict(checkpoint["model_state_dict"])
@@ -204,3 +207,7 @@ class Trainer():
         scheduler.load_state_dict(checkpoint["scheduler_state_dict"])
         losses["train"] = checkpoint["train_loss"]
         losses["valid"] = checkpoint["valid_loss"]
+
+
+if __name__ == "__main__":
+    t = Trainer()
